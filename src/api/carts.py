98201aa1db044f25be2270f57571c83bc859593b
amart89351 <amart89531@gmail.com>
2024-04-08 19:22:16 -0700

    Readability

--- src/api/carts.py
+++ src/api/carts.py
@@ -87,15 +87,6 @@ def post_visits(visit_id: int, customers: list[Customer]):
 @router.post("/")
 def create_cart(new_cart: Customer):
     """ """
-    # sql_to_execute = f"""
-    #     INSERT INTO Cart_Table (Cart_ID, item_sku, Quantity)
-    #     VALUES ({cart_id}, {item_sku}, {cart_item.quantity})
-    #     returning cart_id
-    # """
-
-    # with db.engine.begin() as connection:
-    #     result = connection.execute(sqlalchemy.text(sql_to_execute))
-
 
     return {"cart_id": 1}
 
@@ -108,14 +99,6 @@ class CartItem(BaseModel):
 def set_item_quantity(cart_id: int, item_sku: str, cart_item: CartItem):
     """ """
 
-    # sql_to_execute = f"""
-    #     INSERT INTO Cart_Table (Cart_ID, item_sku, Quantity)
-    #     VALUES ({cart_id}, {item_sku}, {cart_item.quantity})
-    # """
-
-    # with db.engine.begin() as connection:
-    #     result = connection.execute(sqlalchemy.text(sql_to_execute))
-
     return "OK"
 
 
@@ -126,20 +109,6 @@ class CartCheckout(BaseModel):
 def checkout(cart_id: int, cart_checkout: CartCheckout):
     """ """
 
-    # Selecting all rows that match the cart_id
-    # sql_to_execute = f"""
-    #                 SELECT * 
-    #                 FROM Cart_Table
-    #                 WHERE cart_id = {cart_id}
-    #                 """
-
-    # with db.engine.begin() as connection:
-    #     result = connection.execute(sqlalchemy.text(sql_to_execute))
-    #     all_rows = result.fetchall()
-    
-    # # Grabbing total number of green potions
-    # green_potion_count_checkout = sum(all_rows[2])
-
     # Fetch Current Values
     sql_to_execute = "SELECT * FROM global_inventory"
     with db.engine.begin() as connection:
@@ -148,19 +117,6 @@ def checkout(cart_id: int, cart_checkout: CartCheckout):
     
     current_gold = first_row[3]
     current_green_potions = first_row[1]
-
-
-    # Update Number Of Green Potions / Gold (USING MULTIPLE TABLES WILL IMPLEMENT LATER)
-    # sql_to_execute = f"UPDATE global_inventory SET num_green_potions = {current_green_potions - green_potion_count_checkout}"
-    # with db.engine.begin() as connection:
-    #     result = connection.execute(sqlalchemy.text(sql_to_execute))
-
-        
-    # sql_to_execute = f"UPDATE global_inventory SET gold = {current_gold + cart_checkout.payment}"
-    # with db.engine.begin() as connection:
-    #     result = connection.execute(sqlalchemy.text(sql_to_execute))
-    
-    # return {"total_potions_bought": green_potion_count_checkout, "total_gold_paid": cart_checkout.payment}
 
     # Update Number Of Green Potions / Gold
     sql_to_execute = f"UPDATE global_inventory SET num_green_potions = {current_green_potions - 1}"
