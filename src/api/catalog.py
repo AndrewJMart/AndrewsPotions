a63ff6632725dd161d63d5faddaee948e61f5d62
Andrew Martinez Desktop <amart531@calpoly.edu>
2024-04-16 13:45:27 -0700

    only listing potions with quantity greater than 0

--- src/api/catalog.py
+++ src/api/catalog.py
@@ -14,7 +14,7 @@ def get_catalog():
     #TESTED AND WORKS V2.2
 
     # Grab all available potions for catalog
-    select_stocked_potions = f"""SELECT item_sku, SUM(quantity) AS total_potions FROM potion_ledger WHERE total_potions > 0 GROUP BY item_sku"""
+    select_stocked_potions = f"""SELECT item_sku, SUM(quantity) AS total_potions FROM potion_ledger GROUP BY item_sku"""
     with db.engine.begin() as connection:
         result = connection.execute(sqlalchemy.text(select_stocked_potions))
         all_rows = result.fetchall()
@@ -24,19 +24,20 @@ def get_catalog():
 
     # Iterate through each row and append this potion to catalog listing
     for row in all_rows:
-        select_potion = f"""SELECT * FROM potions_table 
-                                 WHERE item_sku = '{row.item_sku}' """
-        with db.engine.begin() as connection:
-            result = connection.execute(sqlalchemy.text(select_potion))
-            potion_metadata = result.fetchone()
+        if row.total_potions > 0:
+            select_potion = f"""SELECT * FROM potions_table 
+                                    WHERE item_sku = '{row.item_sku}' """
+            with db.engine.begin() as connection:
+                result = connection.execute(sqlalchemy.text(select_potion))
+                potion_metadata = result.fetchone()
 
-        # If potion is not already listed
-        catalog_listing.append({
-                    "sku": potion_metadata.item_sku,
-                    "name": potion_metadata.item_sku,
-                    "quantity": row.total_potions,
-                    "price": potion_metadata.price,
-                    "potion_type": [potion_metadata.red, potion_metadata.green, potion_metadata.blue, 0],
-                })
+            # If potion is not already listed
+            catalog_listing.append({
+                        "sku": potion_metadata.item_sku,
+                        "name": potion_metadata.item_sku,
+                        "quantity": row.total_potions,
+                        "price": potion_metadata.price,
+                        "potion_type": [potion_metadata.red, potion_metadata.green, potion_metadata.blue, 0],
+                    })
     
     return catalog_listing
